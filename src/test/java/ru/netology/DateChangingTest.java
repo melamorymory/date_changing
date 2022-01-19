6b0ecbde965a0e6a00a02c529406b5b6cd0e7fb0
Olga Lebedeva <birdswann@gmail.com>
2022-01-19 18:21:42 +0300

    Allure added

--- src/test/java/ru/netology/DateChangingTest.java
+++ src/test/java/ru/netology/DateChangingTest.java
@@ -1,5 +1,8 @@
 package ru.netology;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
@@ -12,6 +15,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.data.DataGenerator.generateDate;
 
 public class DateChangingTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     RegistrationInfo info = DataGenerator.Registration.generateInfo("ru");
 
@@ -33,5 +46,25 @@ public class DateChangingTest {
         $$(".button").find(exactText("Запланировать")).click();
         $$(".button__text").find(exactText("Перепланировать")).click();
         $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + newDate), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+
+    @Test
+    public void testToFail() {
+        String newDate = generateDate(10);
+
+        open("http://localhost:9999");
+        $("[data-test-id='city'] .input__control").setValue(info.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(info.getDate());
+        $x("//*[@name='name']").setValue(info.getName());
+        $x("//*[@name='phone']").setValue(info.getPhone());
+        $(".checkbox__box").click();
+        $$(".button").find(exactText("Запланировать")).click();
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + info.getDate()), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(newDate);
+        $$(".button").find(exactText("Запланировать")).click();
+        $$(".button__text").find(exactText("Перепланировать")).click();
+        $("[data-test-id='success-notification']").shouldHave(text("тест падает для красивого отчёта " + newDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
